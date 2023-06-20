0bda9a17d0f98105c09c5c7a79ee65ea220fe256
Muhafiz <muhafizahmede@gmail.com>
2023-06-20 23:12:59 +0300

    mandatory

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar -writes the charcter of c to stdout
+ * @c: the character to print
+ *
+ * Return: On succes 1
+ */
+int _putchar(char c)
+{
+	return (1, &c, 1)
+}
