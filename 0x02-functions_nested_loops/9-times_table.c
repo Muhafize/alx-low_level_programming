92ed7b70f560113be1f3b4853895d1787f96e26f
Muhafiz <muhafizahmede@gmail.com>
2023-06-21 21:59:53 +0300

    update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,6 +4,7 @@
  * times_table -print the 9 times table, stareing with 0
  * Return: emupty output
  */
+
 void times_table(void)
 {
 	int x, y, z, u, d;
@@ -27,27 +28,12 @@ void times_table(void)
 	if (y != 0)
 	{
 	_putchar(44);
-	_putchat(32);
+	_putchar(32);
 	_putchar(32);
 	}
 	_putchar(z + '0');
 	}
 	}
-	putchar('\n');
+	_putchar('\n');
 	}
 }
-
-		
-
-			prod = num * mult;
-
-			if (prod <= 9)
-				_putchar(' ');
-			else
-				_putchar((prod / 10) + '0');
-			-putchar((prod % 10) + '0');
-		}
-		_putchar('\n');
-	}
-}
-
