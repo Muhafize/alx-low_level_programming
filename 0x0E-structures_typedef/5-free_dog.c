8161cd15f4efca6e63de132e44d9a4303af7bfef
Muhafize <muhafizahmede@gmail.com>
2023-07-18 04:46:07 -0700

    update

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - free dog struct
+ *
+ * @d: struct dog_t
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
