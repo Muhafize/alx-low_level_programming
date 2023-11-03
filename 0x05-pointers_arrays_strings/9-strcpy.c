c129d14c29c8628a39b5df488483090b99749a03
Muhafize <muhafizahmede@gmail.com>
2023-11-02 20:51:03 -0700

    update

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,23 +1,24 @@
 #include "main.h"
+
 /**
- * char *_strcpy - a function that prints n elements of an array
- * @dest: array name
- * @src: is the numbar of elements of the array to be printed
- * Return: a and n inputsi
+ * char *_strcpy - a function that copies the string pointed to by src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
  */
 char *_strcpy(char *dest, char *src)
 {
 	int l = 0;
 	int x = 0;
 
-	while (*(src + 1) != '\0')
+	while (*(src + l) != '\0')
 	{
 		l++;
 	}
-	for ( ; x < 1 ; x++)
+	for ( ; x < l ; x++)
 	{
 		dest[x] = src[x];
 	}
-	dest[1] = '\0';
+	dest[l] = '\0';
 	return (dest);
 }
